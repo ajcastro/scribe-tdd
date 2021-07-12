4db0e7c927f6a9f5107c60080087dd6ce75ef158
ajcastro <ajcastro29@gmail.com>
2021-07-12 12:59:26 +0800

    Fix imports

--- src/Strategies/ResponseFields/GetFromResponseFieldTagFromScribeTdd.php
+++ src/Strategies/ResponseFields/GetFromResponseFieldTagFromScribeTdd.php
@@ -2,6 +2,9 @@
 
 namespace AjCastro\ScribeTdd\Strategies\ResponseFields;
 
+use AjCastro\ScribeTdd\TestResults\RouteTestResult;
+use Knuckles\Camel\Extraction\ExtractedEndpointData;
+use Knuckles\Scribe\Extracting\RouteDocBlocker;
 use Knuckles\Scribe\Extracting\Strategies\ResponseFields\GetFromResponseFieldTag;
 
 class GetFromResponseFieldTagFromScribeTdd extends GetFromResponseFieldTag
